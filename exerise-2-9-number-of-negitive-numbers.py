ae68f71d2b17b852a70a4b84a38122d7341f31f0
rangorstormbron <77683460+rangorstormbron@users.noreply.github.com>
2021-02-08 15:57:41 +0000

    Add files via upload

---
+++ exerise-2-9-number-of-negitive-numbers.py
@@ -0,0 +1,16 @@
+count=0
+number=int(input("please enter a number to add to the count count stops when 0 is entered"))
+while True:
+    number=int(input("please enter a number to add to the count count stops when 0 is entered"))
+    
+
+    if(number ==0):
+        print("total number =" , str(count))
+        break
+
+    if(number <=0):
+        count= count+1
+        continue 
+
+    
+        
